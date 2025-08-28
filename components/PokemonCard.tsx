e5a0d04c2b231c33c110cb88857043535953da6f
majazocom <maja.gedda@zocom.se>
2025-08-28 12:27:12 +0200

    zoom in on poke img

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -33,7 +33,13 @@ interface PokemonCardProps {
 export default function PokemonCard({ pokemon }: PokemonCardProps) {
     return (
         <div className="bg-white p-4 rounded-lg shadow-md w-50 flex flex-col justify-center">
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <div className="relative w-40 h-40 rounded-full overflow-hidden mx-auto border-4 border-amber-500">
+                <img
+                    className="w-full h-full object-cover scale-110"
+                    src={pokemon.sprites.front_default}
+                    alt={pokemon.name}
+                />
+            </div>
             <h2 className="text-2xl font-bold text-center pb-1">{pokemon.name}</h2>
             <div className="flex flex-row gap-1 justify-center">
                 {pokemon.types.map((t) => <TypeBadge key={t.type.name} name={t.type.name} url={t.type.url} />)}
