58b0e7b86d50e995097ccc318f151f910ffa60df
majazocom <maja.gedda@zocom.se>
2025-08-28 13:20:21 +0200

    pokemon card additional styling

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -30,12 +30,14 @@ interface PokemonCardProps {
     pokemon: Pokemon;
 }
 
+// TODO: onclick on card will route to individual pokemon page
+
 export default function PokemonCard({ pokemon }: PokemonCardProps) {
     return (
-        <div className="bg-white p-4 rounded-lg shadow-md w-50 flex flex-col justify-center">
-            <div className={`relative w-40 h-40 rounded-full overflow-hidden mx-auto border-4 cborder-${pokemon.types[0].type.name}`}>
+        <div className="bg-teal-50 p-4 rounded-lg shadow-md w-60 flex flex-col justify-center cursor-pointer transition-all hover:scale-102 border-indigo-400 border-6">
+            <div className={`relative my-6 w-30 h-30 rounded-full overflow-hidden mx-auto border-4 cborder-${pokemon.types[0].type.name}`}>
                 <img
-                    className="w-full h-full object-cover scale-110"
+                    className="w-full h-full object-cover scale-110 bg-white"
                     src={pokemon.sprites.front_default}
                     alt={pokemon.name}
                 />
@@ -47,8 +49,8 @@ export default function PokemonCard({ pokemon }: PokemonCardProps) {
             <div className="flex flex-col gap-2 mt-4">
                 {pokemon.stats.slice(0, 3).map((stat) => (
                     <div key={stat.stat.name} className="flex justify-between items-center w-full">
-                        <p>{stat.stat.name}</p>
-                        <p>{stat.base_stat}</p>
+                        <p className='font-bold'>{stat.stat.name}</p>
+                        <p className='font-bold'>{stat.base_stat}</p>
                     </div>
                 ))}
             </div>
